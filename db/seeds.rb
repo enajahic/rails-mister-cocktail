e5a54bc0e23db6cb483c41c43c72eac295468c6e
ena jahic <enajahicc@gmail.com>
2020-05-07 16:37:45 +0200

    mister coctail app beginning

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+data = JSON.parse(open(url).read)
+
+data["drinks"].each do |ingredient_attributes|
+  Ingredient.create(name: ingredient_attributes["strIngredient1"])
+end
