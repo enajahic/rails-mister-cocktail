9a91334e08fdfce0f98de17196a3909eeed6877a
ena jahic <enajahicc@gmail.com>
2020-05-08 12:54:01 +0200

    micko spp

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Find your coctail", "we have perfect coctail for everybody", "We serve our coctails cold"],
+    strings: ["Find your coctail", "We have perfect coctail for everybody", "We serve our coctails cold"],
     typeSpeed: 50,
     loop: true
   });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,113 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "open-uri"
-require "json"
+Cocktail.destroy_all
+Ingredient.destroy_all
 
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-data = JSON.parse(open(url).read)
+cocktails = [
+  {
+    name: "Sparkling Campari Orange Cocktail",
+    picture: '1.jpeg'
+    },
+  {
+    name: "Classic White Russian",
+    picture: '2.jpeg'
+    },
+  {
+    name: "Whiskey Sour Punch",
+    picture: '3.jpeg'
+    },
+  {
+    name: "Easy Pomegranate Margaritas",
+    picture: '4.jpeg'
+    },
+  {
+    name: "Jameson Old Fashioned",
+    picture: '5.jpeg'
+    },
+  {
+    name: "Pumpkin Apple Shandy",
+    picture: '6.jpeg'
+    },
+  {
+    name: "Sloe Gin Fizz",
+    picture: '29.jpeg'
+    },
+  {
+    name: "Frosting Shots",
+    picture: '27.jpeg'
+    },
+  {
+    name: "Oaxacan Rusty Nail",
+    picture: '9.jpeg'
+    },
+  {
+    name: "Creamsicle Delight Cocktail",
+    picture: '10.jpeg'
+    },
+  {
+    name: "Pink Champagne Sangria",
+    picture: '11.jpeg'
+    },
+  {
+    name: "Irish Cream Pie Cocktail",
+    picture: '12.jpeg'
+    },
+  {
+    name: "Frozen Orange Prozecco",
+    picture: '13.jpeg'
+    },
+  {
+    name: "Mint Julep",
+    picture: '26.jpeg'
+    },
+  {
+    name: "Berry Lemonade Margaritas",
+    picture: '15.jpeg'
+  },
+  {
+    name: "The Bee’s Knees",
+    picture: '16.jpeg'
+    },
+  {
+    name: "Blood Orange Margarita",
+    picture: '17.jpeg'
+    },
+  {
+    name: "Raspberry Rosé Sangria",
+    picture: '18.jpeg'
+    },
 
-data["drinks"].each do |ingredient_attributes|
-  Ingredient.create(name: ingredient_attributes["strIngredient1"])
-end
+    {
+    name: "Blueberry Mojito",
+    picture: '19.jpeg'
+    },
+
+    {
+    name: "Watermelon Infused Vodka",
+    picture: '20.jpeg'
+    },
+    {
+      name: "Skinny Salty Dog",
+      picture: '21.jpeg'
+    },
+
+  {
+    name: "New York Sour Cocktail",
+    picture: '22.jpeg'
+    },
+  {
+    name: "Piña Colada Punch",
+    picture: '23.jpeg'
+  },
+  {
+    name: "Lavender Mojitos",
+    picture: '24.jpeg'
+  }
+]
+
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum lavander watermelon blueberry raspberry whiskey)
+ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
+
+
+cocktails.each { |cocktail| Cocktail.create(cocktail) }
